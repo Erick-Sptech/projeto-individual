2c210478c942631869826bf637c25850b7a1bd08
Erick-Sptech <erick.ferreira@sptech.school>
2025-04-30 00:25:31 -0300

    Melhoria na movimentação do player, adição dos quadros

--- site/script.js
+++ site/script.js
@@ -1,38 +1,71 @@
+// declaração das variáveis
+var player = document.getElementById('player_image')
+var cards = {
+    1: document.getElementById('card1'),
+    2: document.getElementById('card2'),
+    3: document.getElementById('card3'),
+    4: document.getElementById('card4'),
+    5: document.getElementById('card5')
+};
+var playerX = 1
+var playerY = 430
+var cardX = 160
+var cardY = 80
+var cardGap = 30
+var cardWidth = 190
+var cardHeigth = 240
+var esquerda = false
+var direita = false
+const QTD_CARDS = 5
+
+// configuração do canvas
+var galeria = document.getElementById('galeria')
+var ctx = galeria.getContext('2d');
+
+galeria.width = window.innerWidth
+galeria.height = window.innerHeight * (80/100)
+
+window.onload = main();
+
+// verifica se usuario apertou direcional direito/esquerdo
 document.addEventListener('keydown', function(event) {
     console.log('Tecla pressionada:', event.key);
     console.log('Código da tecla:', event.code); // Mais específico, como "KeyA", "Enter", etc.
     console.log('Chave específica:', event.keyCode); // Código numérico (obsoleto, use event.code)
     if (event.key == 'ArrowLeft') {
-        ctx.clearRect(0, 0, galeria.width, galeria.height)
-        andar(-2)
+        esquerda = true
     }
     if (event.key == 'ArrowRight') {
-        ctx.clearRect(0, 0, galeria.width, galeria.height)
-        setInterval(andar(2), 300)
+        direita = true
     }
 });
 
-var player = document.getElementById('player_image')
-var card = document.getElementById('card')
-var playerX = 1
-var playerY = 130
-var cardX = 20
-var cardY = 50
+document.addEventListener('keyup', function(event) {
+        if(event.key == 'ArrowLeft') {
+            esquerda = false
+        }
+        if (event.key == 'ArrowRight') {
+            direita = false
+        }
+})
 
-
-var galeria = document.getElementById('galeria')
-var ctx = galeria.getContext('2d');
-card.src = 'assets/card_image.png'
-
-console.log(card.src)
-
-window.onload = main();
-setInterval(log, 500)
+// Função principal do jogo
+function gameLoop () {
+    if(esquerda) {
+        andar(-2)
+    }
+    if(direita) {
+        andar(2)
+    }
+    printarCanvas()
+    requestAnimationFrame(gameLoop)
+}
+// requestAnimationFrame executa continuamente de acordo com a taxa de atualizção (60 fps) substitui setInterval
+requestAnimationFrame(gameLoop)
 
 function main() {
     ctx.clearRect(0, 0, 100, 100)
-    ctx.drawImage(player, playerX , playerY, 20, 20)
-    ctx.drawImage(card, cardX, cardY, 40, 70)
+    ctx.drawImage(player, playerX , playerY, 80, 80)
 
 }
 
@@ -40,18 +73,32 @@ function log() {
     console.log(playerX)
 }
 
+// função que printa o canvas atualizado
+function printarCanvas() {
+    ctx.clearRect(0, 0, galeria.width, galeria.height)
+    ctx.drawImage(player, playerX, playerY, 80, 80)
+    printarCards()
+}
+
+function printarCards() {
+    console.log('A')
+    for (var i = 1; i <= QTD_CARDS; i++) {
+        let card = cards[i];
+        card.src = `assets/card_image1.png`
+        ctx.drawImage(card, cardX + (i - 1) * (cardWidth + cardGap), cardY, cardWidth, cardHeigth);
+    }
+}
+// função que movimenta o player
 function andar(sentido) {
-    if (playerX < 280 && sentido == 2) {
+    // direita
+    if (playerX < window.innerWidth - 100 && sentido > 0) {
         playerX += sentido
         console.log('<')
     }
-    else if (playerX > 0 && sentido == -2){
+    //esquerda
+    else if (playerX > 0 && sentido < 0){
         playerX += sentido
         console.log('>')
     }
-    ctx.drawImage(player, playerX , playerY, 20, 20)
-}
-
-function andarSmooth() {
-    
+    ctx.drawImage(player, playerX , playerY, 80, 80)
 }
